f3f7b5b05d7eccdb26b710fb8d569787a4250367
ketanishaan <32016802+ketanishaan@users.noreply.github.com>
2017-09-21 08:44:37 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/article-one',function (req,res){
 var counter=0;
 app.get('/counter',function (req,res){
  counter=counter + 1;
- res.send(counter , toString());
+ res.send(counter,toString());
 });
 
 app.get('/article-three',function (req,res){
